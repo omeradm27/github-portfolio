982f45c3bdccd985a78b2bc242a8cd2d338ba54f
Ömer <117517212+omeradm27@users.noreply.github.com>
2025-06-21 04:37:24 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'vite-plugin-windicss'; 
+import tailwindcss from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/github-portfolio/',
+  base: '/github-portfolio',
   plugins: [react(), tailwindcss()],
 });
