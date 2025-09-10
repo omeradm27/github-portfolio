715f264633287f586511beaf51af412a9e34d3ba
Ömer <117517212+omeradm27@users.noreply.github.com>
2025-09-11 01:38:02 +0300

    indexjs updated

--- src/index.jsx
+++ src/index.jsx
@@ -7,3 +7,16 @@ import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<App />)
+
+// Register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
